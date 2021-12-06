5303b950fdb69fb3e04717ea0b914e754d1421a7
qianying guo <qyguo@buaa.edu.cn>
2021-12-06 16:39:42 +0100

    add more branch and isSignal

---
+++ slimNtuple.C
@@ -0,0 +1,293 @@
+#include <iostream>
+#include <set>
+#include <TString.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TMath.h>
+#include <memory>
+#include <string>
+#include <map>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cmath>
+#include <iomanip>
+#include <set>
+
+#define PI 3.14159
+
+// user include files 
+#include "TROOT.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TTree.h"
+#include "TSpline.h"
+#include "TMath.h"
+#include "TRandom3.h"
+#include "TString.h"
+#include "TLorentzVector.h"
+#include "Math/VectorUtil.h"
+#include "TClonesArray.h"
+#include "TCanvas.h"
+
+void slimNtuple(const int & _year_=2017, const string & _name_DS_="GluGluHToZZTo4L_M125_2017", const bool & isSignal = true, const bool & _Test=false) {
+
+    int year = _year_;
+    string pre_name = "root://cmsio5.rc.ufl.edu:1094//store/user/t2/users/ferrico/Qianying/";
+    string whichYear[3] = {"2016/", "2017/", "2018/"};
+    string MC__ = "MC/";
+    //string name_DS = "GluGluHToZZTo4L_M125_2017";
+    string name_DS = _name_DS_.c_str();
+
+    //TString filename = prefix+".root";
+    string filename = (pre_name + whichYear[year-2016] + MC__ + name_DS + ".root").c_str();
+    //filename = "Sync_106X_2018UL_30112021_-1ggH_V2.root";
+
+    std::cout<<"Year: "<<year<<std::endl;
+    std::cout<<filename<<std::endl;
+
+    TFile *oldfile = TFile::Open(filename.c_str());
+    oldfile->cd("Ana");
+    //TTree *oldtree = (TTree*)oldfile->Get("Ana/passedEvents");
+    TTree *oldtree = (TTree*)gDirectory->Get("passedEvents");
+
+    TH1F *th[7];
+    th[0]=(TH1F*)oldfile->Get("Ana/nEvents");
+    th[1]=(TH1F*)oldfile->Get("Ana/sumWeights");
+    th[2]=(TH1F*)oldfile->Get("Ana/sumWeightsPU");
+    th[3]=(TH1F*)oldfile->Get("Ana/nVtx");
+    th[4]=(TH1F*)oldfile->Get("Ana/nVtx_ReWeighted");
+    th[5]=(TH1F*)oldfile->Get("Ana/nInteractions");
+    th[6]=(TH1F*)oldfile->Get("Ana/nInteraction_ReWeighted");
+
+    Long64_t nentries = oldtree->GetEntries();
+    std::cout<<nentries<<" total entries."<<std::endl;
+    //ULong64_t Run, LumiSect, Event;
+    //Bool_t          passedFullSelection;
+    //Float_t         dataMCWeight;
+    //Float_t         eventWeight;
+    //vector<int>     *lep_id;
+    //vector<float>   *lep_pt;
+    //vector<float>   *lep_eta;
+    //Int_t           lep_Hindex[4];
+    //vector<float>   *lep_dataMC;
+    //vector<float>   lep_dataMC_new;
+    //vector<float>   *lep_dataMCErr;
+    //lep_id = 0; 
+    //lep_pt = 0;    
+    //lep_eta = 0;  
+    //lep_dataMC = 0;
+    //lep_dataMCErr = 0;
+    //oldtree->SetBranchAddress("Run",&Run);
+    //oldtree->SetBranchAddress("LumiSect",&LumiSect);
+    //oldtree->SetBranchAddress("Event",&Event);
+    //oldtree->SetBranchAddress("passedFullSelection", &passedFullSelection);
+    //oldtree->SetBranchAddress("dataMCWeight", &dataMCWeight);
+    //oldtree->SetBranchAddress("eventWeight", &eventWeight);
+    //oldtree->SetBranchAddress("lep_id", &lep_id);
+    //oldtree->SetBranchAddress("lep_pt", &lep_pt);
+    //oldtree->SetBranchAddress("lep_Hindex", lep_Hindex);
+    //oldtree->SetBranchAddress("lep_eta", &lep_eta);
+    //oldtree->SetBranchAddress("lep_dataMC", &lep_dataMC);
+    //oldtree->SetBranchAddress("lep_dataMCErr", &lep_dataMCErr);
+    
+    Bool_t passedZ4lSelection;
+    Bool_t passedZXCRSelection;
+    oldtree->SetBranchAddress("passedZ4lSelection", &passedZ4lSelection);
+    oldtree->SetBranchAddress("passedZXCRSelection", &passedZXCRSelection);
+
+    oldtree->SetBranchStatus("*",0); //Disables All Branches
+    //Then enables only select branches
+    oldtree->SetBranchStatus("Run",1);
+    oldtree->SetBranchStatus("LumiSect",1);
+    oldtree->SetBranchStatus("Event",1);
+    oldtree->SetBranchStatus("passedFullSelection",1);
+    oldtree->SetBranchStatus("passedFiducialSelection",1);
+
+    oldtree->SetBranchStatus("passedZ4lSelection",1);
+    oldtree->SetBranchStatus("passedZ1LSelection",1);
+    oldtree->SetBranchStatus("passedZ4lZ1LSelection",1);
+    oldtree->SetBranchStatus("passedZ4lZXCRSelection",1);
+    oldtree->SetBranchStatus("passedZXCRSelection",1);
+    oldtree->SetBranchStatus("nZXCRFailedLeptons",1);
+
+    oldtree->SetBranchStatus("genWeight",1);
+    oldtree->SetBranchStatus("pileupWeight",1);
+    oldtree->SetBranchStatus("prefiringWeight",1);
+    oldtree->SetBranchStatus("dataMCWeight",1);
+    oldtree->SetBranchStatus("eventWeight",1);
+    oldtree->SetBranchStatus("crossSection",1);
+    oldtree->SetBranchStatus("k_qqZZ_qcd_M",1);
+    oldtree->SetBranchStatus("k_qqZZ_ewk",1);
+    oldtree->SetBranchStatus("k_ggZZ",1);
+    oldtree->SetBranchStatus("mass4lREFIT",1);
+
+    oldtree->SetBranchStatus("finalState",1);
+    oldtree->SetBranchStatus("idL1",1);
+    oldtree->SetBranchStatus("idL2",1);
+    oldtree->SetBranchStatus("idL3",1);
+    oldtree->SetBranchStatus("idL4",1);
+    oldtree->SetBranchStatus("pTL1",1);
+    oldtree->SetBranchStatus("pTL2",1);
+    oldtree->SetBranchStatus("pTL3",1);
+    oldtree->SetBranchStatus("pTL4",1);
+    oldtree->SetBranchStatus("etaL1",1);
+    oldtree->SetBranchStatus("etaL2",1);
+    oldtree->SetBranchStatus("etaL3",1);
+    oldtree->SetBranchStatus("etaL4",1);
+    oldtree->SetBranchStatus("pTZ1",1);
+    oldtree->SetBranchStatus("pTZ2",1);
+
+    oldtree->SetBranchStatus("mass4l",1);
+    oldtree->SetBranchStatus("mass2e2mu",1);
+    oldtree->SetBranchStatus("mass4mu",1);
+    oldtree->SetBranchStatus("mass4e",1);
+    oldtree->SetBranchStatus("lep_genindex",1);
+    oldtree->SetBranchStatus("lep_Hindex",1);
+    oldtree->SetBranchStatus("massZ1",1);
+    oldtree->SetBranchStatus("massZ2",1);
+    oldtree->SetBranchStatus("pT4l",1);
+    oldtree->SetBranchStatus("eta4l",1);
+    oldtree->SetBranchStatus("phi4l",1);
+    oldtree->SetBranchStatus("rapidity4l",1);
+    oldtree->SetBranchStatus("Phi",1);
+    oldtree->SetBranchStatus("Phi1",1);
+    oldtree->SetBranchStatus("cosTheta1",1);
+    oldtree->SetBranchStatus("cosTheta2",1);
+    oldtree->SetBranchStatus("cosThetaStar",1);
+    oldtree->SetBranchStatus("njets_pt30_eta4p7",1);
+    oldtree->SetBranchStatus("njets_pt30_eta2p5",1);
+    oldtree->SetBranchStatus("pt_leadingjet_pt30_eta4p7",1);
+    oldtree->SetBranchStatus("pt_leadingjet_pt30_eta2p5",1);
+    oldtree->SetBranchStatus("njets_pt30_eta4p7_jesup",1);
+    oldtree->SetBranchStatus("njets_pt30_eta4p7_jesdn",1);
+    oldtree->SetBranchStatus("pt_leadingjet_pt30_eta4p7_jesup",1);
+    oldtree->SetBranchStatus("pt_leadingjet_pt30_eta4p7_jesdn",1);
+    oldtree->SetBranchStatus("njets_pt30_eta2p5_jesup",1);
+    oldtree->SetBranchStatus("njets_pt30_eta2p5_jesdn",1);
+    oldtree->SetBranchStatus("pt_leadingjet_pt30_eta2p5_jesup",1);
+    oldtree->SetBranchStatus("pt_leadingjet_pt30_eta2p5_jesdn",1);
+
+    if (!_Test) {
+        oldtree->SetBranchStatus("D_0m",1);
+        oldtree->SetBranchStatus("D_CP",1);
+        oldtree->SetBranchStatus("D_0hp",1);
+        oldtree->SetBranchStatus("D_int",1);
+        oldtree->SetBranchStatus("D_L1",1);
+        oldtree->SetBranchStatus("D_L1Zg",1);
+        oldtree->SetBranchStatus("TauC_Inc_0j_EnergyWgt",1);
+        oldtree->SetBranchStatus("TauB_Inc_0j_pTWgt",1);
+        oldtree->SetBranchStatus("pTj2",1);
+        oldtree->SetBranchStatus("pTj2_jesup",1);
+        oldtree->SetBranchStatus("pTj2_jesdn",1);
+        oldtree->SetBranchStatus("mj1j2",1);
+        oldtree->SetBranchStatus("mj1j2_jesup",1);
+        oldtree->SetBranchStatus("mj1j2_jesdn",1);
+        oldtree->SetBranchStatus("dEtaj1j2",1);
+        oldtree->SetBranchStatus("dEtaj1j2_jesup",1);
+        oldtree->SetBranchStatus("dEtaj1j2_jesdn",1);
+        oldtree->SetBranchStatus("dPhij1j2",1);
+        oldtree->SetBranchStatus("dPhij1j2_jesup",1);
+        oldtree->SetBranchStatus("dPhij1j2_jesdn",1);
+        oldtree->SetBranchStatus("pT4lj",1);
+        oldtree->SetBranchStatus("pT4lj_jesup",1);
+        oldtree->SetBranchStatus("pT4lj_jesdn",1);
+        oldtree->SetBranchStatus("mass4lj",1);
+        oldtree->SetBranchStatus("mass4lj_jesup",1);
+        oldtree->SetBranchStatus("mass4lj_jesdn",1);
+        oldtree->SetBranchStatus("pT4ljj",1);
+        oldtree->SetBranchStatus("pT4ljj_jesup",1);
+        oldtree->SetBranchStatus("pT4ljj_jesdn",1);
+        oldtree->SetBranchStatus("mass4ljj",1);
+        oldtree->SetBranchStatus("mass4ljj_jesup",1);
+        oldtree->SetBranchStatus("mass4ljj_jesdn",1);
+    }
+
+    oldtree->SetBranchStatus("GENZ_DaughtersId",1);
+    oldtree->SetBranchStatus("GENZ_MomId",1);
+    oldtree->SetBranchStatus("GENlep_MomMomId",1);
+    oldtree->SetBranchStatus("GENlep_MomId",1);
+    oldtree->SetBranchStatus("GENlep_Hindex",1);
+    oldtree->SetBranchStatus("GENlep_id",1);
+
+    oldtree->SetBranchStatus("GENmass4l",1);
+    oldtree->SetBranchStatus("GENmassZ1",1);
+    oldtree->SetBranchStatus("GENmassZ2",1);
+    oldtree->SetBranchStatus("GENpT4l",1);
+    oldtree->SetBranchStatus("GENeta4l",1);
+    oldtree->SetBranchStatus("GENphi4l",1);
+    oldtree->SetBranchStatus("GENrapidity4l",1);
+    oldtree->SetBranchStatus("GENPhi",1);
+    oldtree->SetBranchStatus("GENPhi1",1);
+    oldtree->SetBranchStatus("GENcosTheta1",1);
+    oldtree->SetBranchStatus("GENcosTheta2",1);
+    oldtree->SetBranchStatus("GENcosThetaStar",1);
+    oldtree->SetBranchStatus("GENnjets_pt30_eta4p7",1);
+    oldtree->SetBranchStatus("GENnjets_pt30_eta2p5",1);
+    oldtree->SetBranchStatus("GENpt_leadingjet_pt30_eta4p7",1);
+    oldtree->SetBranchStatus("GENpt_leadingjet_pt30_eta2p5",1);
+
+    if (!_Test) {
+        oldtree->SetBranchStatus("GEND_0m",1);
+        oldtree->SetBranchStatus("GEND_CP",1);
+        oldtree->SetBranchStatus("GEND_0hp",1);
+        oldtree->SetBranchStatus("GEND_int",1);
+        oldtree->SetBranchStatus("GEND_L1",1);
+        oldtree->SetBranchStatus("GEND_L1Zg",1);
+        oldtree->SetBranchStatus("GEN_TauC_Inc_0j_EnergyWgt",1);
+        oldtree->SetBranchStatus("GEN_TauB_Inc_0j_pTWgt",1);
+
+        oldtree->SetBranchStatus("GENpTj2",1);
+        oldtree->SetBranchStatus("GENmj1j2",1);
+        oldtree->SetBranchStatus("GENdEtaj1j2",1);
+        oldtree->SetBranchStatus("GENdPhij1j2",1);
+        oldtree->SetBranchStatus("GENpT4lj",1);
+        oldtree->SetBranchStatus("GENmass4lj",1);
+        oldtree->SetBranchStatus("GENpT4ljj",1);
+        oldtree->SetBranchStatus("GENmass4ljj",1);
+    }
+
+    //Create a new file + a clone of old tree in new file
+    TFile *newfile = new TFile(
+            (name_DS+"_slimmed.root").c_str()
+            ,"recreate");
+    cout<<"Output file: "<<newfile->GetName()<<endl;
+    newfile->mkdir("Ana");
+    newfile->cd("Ana");
+    for (int ii=0; ii<7; ii++)
+    {
+      th[ii]->Write();
+    }
+    TTree *newtree = oldtree->CloneTree(0);
+    //newtree->Branch("dataMCWeight_new", &dataMCWeight_new);
+    //newtree->Branch("eventWeight_new", &eventWeight_new);
+
+    std::set<TString> runlumieventSet;
+    for (Long64_t i=0;i<nentries; i++) {
+        //lep_dataMC_new.clear();
+        //if (i>=2000000) continue;
+        if (i>=2000000&&_Test) break;
+        if (i%100000==0) std::cout<<i<<"/"<<nentries<<std::endl;
+        oldtree->GetEntry(i);
+        if (isSignal)
+        {
+            newtree->Fill();
+        }
+        if (!isSignal && (passedZ4lSelection ||passedZXCRSelection))
+        {
+            newtree->Fill();////if only store the event passedFullSelection
+        }
+        
+    }
+
+    newtree->AutoSave();
+    newfile->Close();
+    oldfile->Close();
+    //delete oldfile;
+    delete newfile;
+    exit(EXIT_FAILURE);
+}
+
