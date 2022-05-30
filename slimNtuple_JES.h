a1c2f4641de3dc9cb82d8630ec8d9d411e60c5f5
Tahir Javaid <jtahir@ihep.ac.cn>
2022-05-30 16:51:40 +0800

    inclusion of JES split branches

---
+++ slimNtuple_JES.h
@@ -0,0 +1,7 @@
+#ifndef slimNtuple_JES_h
+    #define slimNtuple_JES_h
+    
+    /*function declaration.*/
+    void slimNtuple_JES(const int & _year_, const string & _name_DS_, const bool & isMC ,  const bool & isSignal, const bool & _Test, const bool & applyJEC_ );
+    
+#endif
---
+++ test_run.sh
@@ -0,0 +1,1 @@
+root -l -b -q slimNtuple_JES.C+\(2016,\"ttH_HToZZ_4LFilter_M124_TuneCP5_13TeV_powheg2_JHUGenV7011_pythia8\",true,true,false,true\)
